2fdec8be8cd3e6365a9f942d1fad94ce04855879
Brian Armstrong <dr.ductus@gmail.com>
2015-07-01 02:04:32 -0600

    bed time

--- goaltracker.js
+++ goaltracker.js
@@ -1,4 +1,4 @@
-angular.module('GoalTracker',['ui.bootstrap'])
+angular.module('GoalTracker', ['ui.bootstrap'])
 .factory('gtPersist', function(){
 	var prefix = 'gt_';
 
@@ -19,10 +19,37 @@ angular.module('GoalTracker',['ui.bootstrap'])
 
 
 function GoalTrackerCtrl($scope, $modal, gtPersist) {
-	var modalInstance = $scope.showAddModal = function() {
-		$modal.open({
+	$scope.goals = [];
+
+	function _validateGoal(name, wordCount, daysRemaining) {
+		if (angular.isUndefined(name) || name.length == 0)
+			throw 'Goal name cannot be empty';
+
+		if (angular.isUndefined(wordCount) || !Number.isFinite(wordCount) || wordCount <= 0)
+			throw 'Word Count must be a number greater than 0';
+
+		if (angular.isUndefined(daysRemaining) || !Number.isFinite(daysRemaining) || daysRemaining <= 0)
+			throw 'Days Remaining must be a number greater than 0';
+	}
+
+	function _addGoal(name, wordCount, daysRemaining) {
+		_validateGoal(name, wordCount, daysRemaining);
+
+		// TODO save
+	}
+
+	$scope.showAddModal = function() {
+		var modalInstance = $modal.open({
 			templateUrl: 'partial/goal_dialog.html',
 			controller: 'AddGoalCtrl'
+		});
+
+		modalInstance.result.then(function(goal) {
+			try {
+				_addGoal(name, wordCount, daysRemaining);
+			} catch (error) {
+				// TODO handle error
+			}
 		});
 	};
 }
@@ -31,8 +58,7 @@ function AddGoalCtrl($scope, $modalInstance) {
 	$scope.cancel = $modalInstance.dismiss;
 
 	$scope.save = function(goal) {
-		console.log(goal);
-		return false;
+		$modalInstance.close(goal);
 	};
 }
 

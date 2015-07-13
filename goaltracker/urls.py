45bcf20b64afd8662566a6c8359756db0bd08196
shaun <shaun@sdrichins.com>
2015-07-13 12:21:07 -0600

    Basic urls and some views

--- goaltracker/urls.py
+++ goaltracker/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include, patterns, url
 from django.contrib import admin
 
-urlpatterns = [
+from . import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+    url(r'^api/goals/', include('goals.urls')),
     url(r'^admin/', include(admin.site.urls)),
-]
+)
